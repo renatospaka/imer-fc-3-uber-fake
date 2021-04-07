7cd02e5754294c25e21d5d026bab6a324d4d1544
Renato Spakauskas <renatospaka@gmail.com>
2021-04-07 20:11:17 -0300

    feat: :construction: route selection

--- src/components/Mapping.tsx
+++ src/components/Mapping.tsx
@@ -1,18 +1,36 @@
 import { Grid, MenuItem, Select, Button } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import { Route } from "../util/models";
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 type Props = {};
 export const Mapping = (props: Props) => {
+  const [routes, setRoutes] = useState<Route[]>([]);
+  const [routeIdSelected, setRouteIdSelected] = useState<string>("");
+  
+  useEffect(() => {
+    fetch(`${API_URL}/routes`)
+      .then((data) => data.json())
+      .then((data) => setRoutes(data))
+  }, [])
+
   return (
     <Grid container>
       <Grid item xs={12} sm={3}>
         <form>
-          <Select fullWidth >
-            <MenuItem value="">
-              <em>Selecione uma corrida</em>
-            </MenuItem>
-            <MenuItem value="1">
-              <em>Primeira</em> 
-            </MenuItem>
+          <Select
+            fullWidth
+            displayEmpty
+            value={routeIdSelected}
+            onChange={(event) => setRouteIdSelected(event.target.value + "")}
+          >
+            <MenuItem value="">Selecione uma corrida</MenuItem>
+            {routes.map((route, key) => (
+              <MenuItem key={key} value={route._id}>
+                <em>{route.title}</em>
+              </MenuItem>
+            ))}
           </Select>
           <Button type="submit" color="primary" variant="contained">
             Iniciar uma corrida.
